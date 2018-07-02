9bd6483c7fb83008dfe005ce0a41719dfdff8ab9
cornkle <cornelia.klein@gmx.de>
2018-07-02 12:17:37 +0100

    fixed CP4 data extraction

--- JASMIN/JASMIN_extract_script_CLOVER.py
+++ JASMIN/JASMIN_extract_script_CLOVER.py
@@ -3,75 +3,85 @@ import glob
 import os
 import itertools
 from JASMIN import constants as cnst, MetUM_variables as mv
+import numpy as np
 import pdb
+fpath = '/users/global/cornkle/figs/CLOVER/CP4testfiles'
+
+def run(fpath, orig_names=False):
 
 
-#veg = '/users/global/cornkle/w2018_bamba/qrparm.cci.4km.nc'
+    local_box = [-18+360,14+360, 3.5, 14]
+    temp_box = [-18+360,35+360, 3.5, 30]
 
-### 2d vars , xmh*.pc*.nc files
-folder = '/users/global/cornkle/figs/CLOVER/CP4testfiles'
+    months = [3,5] # March-May
+    month_tag = 'MAM'
 
-local_box = [-18+360,14+360, 3.5, 14]
-temp_box = [-18+360,48+360, -36, 32]
+    dic = {
 
-months = [3,5] # March-May
+        't2' : ([temp_box], ['keep'], [], [12,3]),
+        'lw_out_PBLtop' : ([local_box], ['keep'], [], []),
+        'u_pl' : ([temp_box], ['keep', 'keep'], [650, 850], [12,3]),
+        't_pl' : ([temp_box], ['keep'], [925], [12,3]),
+        'omega_pl' : ([local_box], ['keep'], [650,300], []),
+        'lsRain' : ([local_box], ['keep'], [], []),
 
-dic = {
+    }
+    keys = dic.keys()
 
-    't2_daily' : ([temp_box], ['keep'], []),
-    'lw_out_PBLtop' : ([local_box], ['keep'], []),
-    'u_pl' : ([temp_box, local_box], ['agg', 'keep'], [650, 850]),
-    't_pl' : ([local_box], ['agg'], [925]),
-    'omega_pl' : ([local_box], ['keep'], []),
-    'lsRain' : ([local_box], ['keep'], []),
+    for k in keys:
 
-}
-keys = dic.keys()
+        info = cnst.VARDIC[k]
+        dinfo = dic[k]
+        if orig_names:
+            var = mv.create_CP4_filename(k)
+        else:
+            var = k
+        infolder = fpath+os.sep + var
+        outfolder = fpath +os.sep +  k
+        files = glob.glob(infolder + os.sep + var+'*.nc' )
 
-for k in keys:
+        for f in files:
 
-    info = cnst.VARDIC[k]
-    dinfo = dic[k]
-    var = mv.create_CP4_filename(k)
-    infolder = folder+os.sep + var
-    outfolder = folder +os.sep +  k
-    files = glob.glob(infolder + os.sep + var+'*.nc' )
+            ds = xr.open_dataset(f)
 
-    for f in files:
+            if (ds['time.month'][0]<months[0]) | (ds['time.month'][0]>months[1]):
+                continue
 
-        ds = xr.open_dataset(f)
+            if dinfo[3] != []:
+                ds = ds.isel(time=[np.in1d(ds['time.hour'].values, dinfo[3])][0])
+                pdb.set_trace()
 
-        if (ds['time.month'][0]<months[0]) | (ds['time.month'][0]>months[1]):
-            continue
+            fname = os.path.basename(f)
+            box = dinfo[0]
 
-        fname = os.path.basename(f)
-        box = dinfo[0]
+            for id, b in enumerate(box):
 
-        for id, b in enumerate(box):
+                agg = dinfo[1][id]
+                pres = dinfo[2]
+                cut = ds.sel(longitude=slice(b[0], b[1]), latitude=slice(b[2], b[3]))
+                da = cut[var]
 
-            agg = dinfo[1][id]
-            pres = dinfo[2]
-            cut = ds.sel(longitude=slice(b[0], b[1]), latitude=slice(b[2], b[3]))
-            da = cut[var]
+                if pres != []:
+                    da = da.sel(pressure=pres)
 
-            if pres != []:
-                da = da.sel(pressure=pres)
+                if agg != 'keep':
+                    da = da.resample('24H', base=16, dim='time', skipna=True, how='mean')
 
-            # if agg != 'keep':
-            #     da = da.resample('d', dim='time', how='mean')
+                outname = fname.replace(var, k+'_'+month_tag)
 
-            outname = fname.replace(var, k)
+                comp = dict(zlib=True, complevel=5)
 
-            comp = dict(zlib=True, complevel=5)
+                da.name = k
+                da.longitude.values = da.longitude.values-360
+                #encoding = {var: comp for var in da.data_vars}
+                encoding = {k: {'complevel': 5, 'zlib': True}}
+                if not os.path.exists(outfolder):
+                    os.makedirs(outfolder)
 
-            da.name = k
-            #encoding = {var: comp for var in da.data_vars}
-            encoding = {k: {'complevel': 5, 'zlib': True}}
-            if not os.path.exists(outfolder):
-                os.makedirs(outfolder)
+                da.to_netcdf(outfolder + os.sep + outname , format='NETCDF4', encoding=encoding)
+                da.close()
 
-            da.to_netcdf(outfolder + os.sep + outname , format='NETCDF4', encoding=encoding)
-            da.close()
+                print('Wrote '+ outfolder + os.sep + outname)
 
 
 
