a25e7529821ec70524d287088aa5e52a8d775d0f
Cornelia Klein <cornelia.klein@gmx.de>
2023-03-12 16:38:15 +0000

    new constants paths between notebook and network

--- utils/constants.py
+++ utils/constants.py
@@ -2,54 +2,43 @@ import os
 
 ########## FILE PATHS
 if os.path.isdir('/home/ck/DIR/'):
-    lappi_drive = '/home/ck/DIR/'
-    local_data = lappi_drive + 'mymachine/'
-    network_data = lappi_drive + 'cornkle/'
-    ext_drive = '/media/ck/LStorage/'
-    lmcs_drive = ext_drive + 'global_water/'
+    lappi_drive = '/home/ck/DIR/'  # backup on google drive for full drive. This where own data goes
+    network_data = lappi_drive + 'cornkle/' # 'shared' on CEH network
+    ext_drive = '/media/ck/LStorage/' # most of global_water project goes here / external drive
+    lmcs_drive = ext_drive + 'global_water/' # specifically lmcs stuff
+    other_drive = ext_drive + 'global_water/other/' # data from other projects
+    scratch = ext_drive # scratch does not exist locally, so save on external drive
 
 
 else:
     if os.path.isdir('/prj/global_water/'):
-        scratch = '/scratch/cornkle/'
-        network_data = '/users/global/cornkle/shared/'  # private U drive i.e. laptop on CEH network
-        lmcs_drive = '/prj/global_water/'  # "external drive" on CEH network
-        ext_drive = '/prj/global_water/'
-        local_data = '/prj/global_water/'
+        scratch = '/scratch/cornkle/'  # scratch in CEH network
+        network_data = '/users/global/cornkle/shared/'  # private U drive , equivalent of /cornkle/ on lappi
+        lmcs_drive = '/prj/global_water/'  # lmcs stuff
+        ext_drive = lmcs_drive # no external drive on CEH network, links to global_water proj
+        other_drive = 'prj/global_water/other/' # other project's big files / external drive locally
 
 
 ANCILS = network_data + 'data/ancils_python/'
 
-
-# ERA_MONTHLY_PL = local_data + 'ERA-I/monthly/monthly_1979-2017_pl_full.nc'
-# ERA_MONTHLY_SRFC = local_data + 'ERA-I/monthly/monthly_1979-2017_srfc_full.nc'
-# ERA_MONTHLY_PL_SYNOP = local_data + 'ERA-I/monthly/monthly_synop_1979-2017_pl_full.nc'
-# ERA_MONTHLY_SRFC_SYNOP = local_data + 'ERA-I/monthly/monthly_synop_1979-2017_srfc_full.nc'
-# ERA_DAILY_SURFACE =   local_data + 'ERA-I/daily_2006-2010_12UTCsrfc.nc'
-# ERA_MONTHLY_TUVWZ_AFRICA = local_data + 'ERA-I/climatology/ERA-Int-MonthlyAvg-4D-TUVWZ_Africa.nc'
-
-#ERA_DAILY_PL12UTC = local_data + 'ERA-I/daily_1983-2014_pl.nc'
-# ERA_DAILY_PL_NIGHT = '/localscratch/wllf030/cornkle/ERA-I/daily_2006-2010_12UTCpl_night.nc'
-# ERA_DAILY_SRFC_ANO = '/localscratch/wllf030/cornkle/ERA-I/daily_2006-2010_12UTCsrfc_anomaly.nc'
-#ERA5 = local_data + 'ERA5/'
-#ERA5_MONTHLY_PL_SYNOP = local_data + 'ERA5/monthly/synoptic/pl_1979-2019_monthly_synop_07x07.nc'
-#ERA5_MONTHLY_SRFC_SYNOP = local_data + 'ERA5/monthly/synoptic/srfc_1979-2019_monthly_synop_07x07.nc'
-
-#
-# ERA5_MONTHLY_PL_SYNOP_HU = elements_drive + 'SouthAmerica/ERA5/monthly/pressure_levels/synop'
-# ERA5_HOURLY_PL_HU = elements_drive + 'SouthAmerica/ERA5/hourly/pressure_levels/'
-# ERA5_HOURLY_SRFC_HU = elements_drive + 'SouthAmerica/ERA5/hourly/surface/'
-
-#ERA5_HOURLY_SRFC = local_data + 'ERA5/hourly/surface/'
-#ERA5_HOURLY_PL = local_data +'ERA5/hourly/pressure_levels/'
+DATA = network_data+'data/'
+FIGS = network_data+'figs/'
 
 
-LSTA_TOPO = network_data + 'data/ancils_python/lsta_corr_topo.nc'
-LSTA_TOPO_OLD = network_data + 'data/ancils_python/lsta_topo.nc'
-WA_TOPO_1MIN = network_data + 'data/ancils_python/gtopo_1min_afr.nc'
-TOPO_1MIN = network_data + 'data/ancils_python/gtopo_1min.nc'
-MSG5KM_TOPO = network_data + 'data/ancils_python/msg5km_topo.nc'
-WA_TOPO_3KM = network_data + 'data/ancils_python/gtopo_3km_WA.nc'
+#West Africa ERA5
+ERA5_HOURLY_SRFC = DATA + 'ERA5/hourly/surface/'
+ERA5_HOURLY_PL = DATA +'ERA5/hourly/pressure_levels/'
+ERA5 = DATA + 'ERA5/'
+ERA5_MONTHLY_PL_SYNOP = DATA + 'ERA5/monthly/synoptic/pl_1979-2019_monthly_synop_07x07.nc'
+ERA5_MONTHLY_SRFC_SYNOP = DATA + 'ERA5/monthly/synoptic/srfc_1979-2019_monthly_synop_07x07.nc'
+
+
+LSTA_TOPO = ANCILS + 'lsta_corr_topo.nc'
+LSTA_TOPO_OLD = ANCILS + 'lsta_topo.nc'
+WA_TOPO_1MIN = ANCILS + 'gtopo_1min_afr.nc'
+TOPO_1MIN = ANCILS + 'gtopo_1min.nc'
+MSG5KM_TOPO = ANCILS + 'msg5km_topo.nc'
+WA_TOPO_3KM = ANCILS + 'gtopo_3km_WA.nc'
 
 
 MCS_POINTS_DOM = network_data + 'MCSfiles/blob_map_allscales_-50_JJAS_points_dominant.nc' #/users/global/cornkle/MCSfiles/blob_map_MCSs_-40-75000_JJAS_-50-points_dominant.nc'#'/users/global/cornkle/MCSfiles/blob_map_allscales_-50_JJAS_points_dominant.nc'
@@ -63,7 +52,7 @@ MCS_CENTRE70_GT5 = network_data + 'MCSfiles/blob_map_JJAS_-70CentreMass_GT5k.nc'
 MCS_CENTRE40 = network_data + 'MCSfiles/blob_map_JJAS_-40CentreMass_25k.nc'
 MCS_BACK = network_data + 'MCSfiles/blob_map_JJAS_25000-40_-70CentreMassBack.nc'
 
-CP4_PATH= network_data + 'data/CP4/'
+CP4_PATH= DATA + '/CP4/'
 CP4_OLR = CP4_PATH + 'CLOVER/CP4hist/lw_out_PBLtop/lw_out_PBLtop_MAM_A1hr_mean_ah261_4km_200205040030-200205042330.nc'
 CP4_RAIN = CP4_PATH + 'CLOVER/CP4hist/lsRain/lsRain_MAM_A1hr_mean_ah261_4km_200205040030-200205042330.nc'
 CP4_LANDSEA = CP4_PATH + 'ANCILS/landseamask_ancil_4km.nc'
@@ -74,14 +63,14 @@ CLOVER_SAVES = network_data + '/data/CLOVER/saves/'
 
 ########## DIRECTORIES
 
-LSTA = network_data + 'data/OBS/MSG_LSTA/lsta_netcdf/'
-LSTA_NEW = network_data + 'data/OBS/MSG_LSTA/lsta_netcdf_new/'  # lsta_netcdf_new
-LSTA_1330 = network_data + 'data/OBS/MSG_LSTA/lsta_netcdf_1330/'
-LSTA_NEW_MEAN = network_data + 'data/OBS/MSG_LSTA/lsta_netcdf_new_-mean/'
+LSTA = DATA + 'OBS/MSG_LSTA/lsta_netcdf/'
+LSTA_NEW = DATA + 'OBS/MSG_LSTA/lsta_netcdf_new/'  # lsta_netcdf_new
+LSTA_1330 = DATA + 'OBS/MSG_LSTA/lsta_netcdf_1330/'
+LSTA_NEW_MEAN = DATA + 'OBS/MSG_LSTA/lsta_netcdf_new_-mean/'
 LSTA_TESTFILE = LSTA_NEW + 'lsta_daily_20060624.nc'
-LSTA_DOMSCALE = network_data + 'data/OBS/MSG_LSTA/power_maps/'
+LSTA_DOMSCALE = network_data + 'OBS/MSG_LSTA/power_maps/'
 
-AMSRE_DIR = network_data + 'data/OBS/AMSRE/aqua/'
+AMSRE_DIR = DATA + 'OBS/AMSRE/aqua/'
 AMSRE_NIGHT = AMSRE_DIR + 'nc_night/'
 AMSRE_DAY = AMSRE_DIR + 'nc_day/'
 AMSRE_ANO_NIGHT = AMSRE_DIR + 'sma_nc_night_new/'
@@ -93,13 +82,21 @@ AMSRE_NIGHT_TESTFILE = AMSRE_NIGHT + 'AMSR_L3_LPRMv05_A_20060116.nc'
 TRMM5KM = network_data + 'TRMMfiles/'
 TRMM5KM_FILE = TRMM5KM + 'TRMM5km_2006-2010.nc'
 
-CMORPH = network_data + 'data/OBS/CMORPH/CMORPH_nc/'
+CMORPH = DATA + 'OBS/CMORPH/CMORPH_nc/'
 
-#GRIDSAT = local_data + 'GRIDSAT/MCS18/'
-#GRIDSAT_PERU = local_data + 'GRIDSAT/MCS18_peru/'
-#GRIDSAT_RAW = local_data + 'GRIDSAT/'
-CHIRPS = network_data + 'data/OBS/CHIRPS/'
-CHIRPS_MONTHLY = network_data + 'data/OBS/CHIRPS/monthly/'
+GRIDSAT = DATA + 'GRIDSAT/MCS18/'
+GRIDSAT_PERU = DATA + 'GRIDSAT/MCS18_peru/'
+GRIDSAT_RAW = DATA + 'GRIDSAT/'
+CHIRPS = DATA + 'OBS/CHIRPS/'
+CHIRPS_MONTHLY = DATA + 'OBS/CHIRPS/monthly/'
+
+
+
+########### Lost in elements head crash:
 
 # TRMM_DAILY = elements_drive + 'TRMM/data/daily/aggregated/'
 # TRMM_MONTHLY = elements_drive + 'TRMM/data/monthly/aggregated/'
+
+# ERA5_MONTHLY_PL_SYNOP_HU = elements_drive + 'SouthAmerica/ERA5/monthly/pressure_levels/synop'
+# ERA5_HOURLY_PL_HU = elements_drive + 'SouthAmerica/ERA5/hourly/pressure_levels/'
+# ERA5_HOURLY_SRFC_HU = elements_drive + 'SouthAmerica/ERA5/hourly/surface/'
