829ca7e50a3c30897b2a9629e57b052ebf565940
Cornelia Klein <cornelia.klein@gmx.de>
2023-06-08 21:27:32 +0100

    LMCS shear/tgrad ERA5 relationships

---
+++ ERA-I_downloads/ERA5_monthly_synop_SouthAfrica.py
@@ -0,0 +1,43 @@
+import cdsapi
+
+def download(year):
+    c = cdsapi.Client()
+
+    c.retrieve(
+        'reanalysis-era5-single-levels-monthly-means',
+        {
+            'format':'netcdf',
+            'product_type':'monthly_averaged_reanalysis_by_hour_of_day',
+            'variable':[
+                '100m_u_component_of_wind','100m_v_component_of_wind','10m_u_component_of_wind',
+                '10m_v_component_of_wind','2m_dewpoint_temperature',
+                '2m_temperature','boundary_layer_height','convective_available_potential_energy',
+                'convective_inhibition','high_cloud_cover',
+                'mean_sea_level_pressure',
+                'surface_latent_heat_flux','surface_pressure','surface_sensible_heat_flux',
+                'total_column_water_vapour','vertical_integral_of_divergence_of_moisture_flux',
+                'vertically_integrated_moisture_divergence'
+            ],
+            'year':[str(year)],
+            'month':[
+                '01','02','03',
+                '04','05','06',
+                '07','08','09',
+                '10','11','12'
+            ],
+            'area': '5/0/-40/90',#'25/-18.5/3.5/17',  # pick domain upper/left/lower/right
+            'grid': '0.7/0.7',
+            'time': [
+                '00:00',  '03:00',
+                 '06:00',
+                '09:00',
+                 '12:00', '15:00',
+                '18:00',
+                '21:00'
+            ]
+        },
+        '/prj/AMMA2050/ERA5/monthly/SA/synoptic/surface/ERA5_monthly_srfc_'+str(y)+'_synop.nc')
+
+for y in range(1979,2020):
+    print('Doing year', y)
+    download(y)
--- GLOBAL/GLOBAL_CMIP6-LS3MIP-30ySlicing.py
+++ GLOBAL/GLOBAL_CMIP6-LS3MIP-30ySlicing.py
@@ -6,7 +6,7 @@ import sys
 
 
 cmip_models = ['MPI-ESM1-2-LR', 'CESM2', 'IPSL-CM6A-LR', 'CNRM-CM6-1', 'EC-Earth3']
-experiments = {'historical' : (1980,2007), 'ssp585': (2080,2101), 'amip-lfmip-pdLC_hist' : (1980,2007), 'amip-lfmip-pdLC_fut' : (2080,2101)}
+experiments = {'amip-lfmip-rmLC_hist' : (1980,2007), 'amip-lfmip-rmLC_fut' : (2080,2101), 'historical' : (1980,2007), 'ssp585': (2080,2101), 'amip-lfmip-pdLC_hist' : (1980,2007), 'amip-lfmip-pdLC_fut' : (2080,2101)}
 mtag = sys.argv[1]
 VARS=['tas', 'ua', 'va', 'prw', 'hfls', 'hfss', 'pr', 'psl', 'tasmax', 'tasmin'] # tasmax tasmin, missing in CESM2 historical
 #MODEL = cmip_models[1]
@@ -17,12 +17,14 @@ for MODEL in cmip_models:
         print(vv)
         for exp in experiments:
             print(exp)
-            if "amip" in exp:
+            if "rmLC" in exp:
+                exp_read = 'amip-lfmip-rmLC'
+            elif "pdLC" in exp:
                 exp_read = 'amip-lfmip-pdLC'
             else:
                 exp_read = exp
             path = cnst.lmcs_drive+'CMIP6/LS3MIP/'+exp_read+'/'+vv+'_*_'+MODEL+'*.nc'
-            opath = cnst.lmcs_drive+'CMIP6/slices_30y/'+exp_read+'/'
+            opath = cnst.lmcs_drive+'CMIP6/LS3MIP/slices_30y/'+exp_read+'/'
             outpath = opath + vv+'_30y_'+mtag+'_'+str(experiments[exp][0])+'-'+str(experiments[exp][1])+'_'+MODEL+'.nc'
 
             if os.path.isfile(outpath):
@@ -39,6 +41,10 @@ for MODEL in cmip_models:
                 ds = ds.sel(time=(ds['time.month']>=12) | (ds['time.month']<=2))
             if mtag == 'JJA':
                ds = ds.sel(time=(ds['time.month']>=6) & (ds['time.month']<=8))
+            if mtag == 'MAM':
+               ds = ds.sel(time=(ds['time.month']>=3) & (ds['time.month']<=5))
+            if mtag == 'SON':
+               ds = ds.sel(time=(ds['time.month']>=9) & (ds['time.month']<=11))
             uni = np.unique(ds['time.year'])
             print(exp, 'year slice', uni[0],uni[-1])
             print('months', np.unique(ds['time.month']))
