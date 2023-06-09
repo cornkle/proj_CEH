efeccb1f6b7be2d6f333c1f30562580374aac8b9
Cornelia Klein <cornelia.klein@gmx.de>
2023-06-09 08:43:09 +0100

    CMIP slicing again

--- GLOBAL/GLOBAL_CMIP6-LS3MIP-30ySlicing.py
+++ GLOBAL/GLOBAL_CMIP6-LS3MIP-30ySlicing.py
@@ -11,49 +11,50 @@ mtag = sys.argv[1]
 VARS=['tas', 'ua', 'va', 'prw', 'hfls', 'hfss', 'pr', 'psl', 'tasmax', 'tasmin'] # tasmax tasmin, missing in CESM2 historical
 #MODEL = cmip_models[1]
 MPI = {}
-for MODEL in cmip_models:
-    print(MODEL)
-    for vv in VARS:
-        print(vv)
-        for exp in experiments:
-            print(exp)
-            if "rmLC" in exp:
-                exp_read = 'amip-lfmip-rmLC'
-            elif "pdLC" in exp:
-                exp_read = 'amip-lfmip-pdLC'
-            else:
-                exp_read = exp
-            path = cnst.lmcs_drive+'CMIP6/LS3MIP/'+exp_read+'/'+vv+'_*_'+MODEL+'*.nc'
-            opath = cnst.lmcs_drive+'CMIP6/LS3MIP/slices_30y/'+exp_read+'/'
-            outpath = opath + vv+'_30y_'+mtag+'_'+str(experiments[exp][0])+'-'+str(experiments[exp][1])+'_'+MODEL+'.nc'
+for mtag in ['DJF','MAM','JJA','SON']:
+    for MODEL in cmip_models:
+        print(MODEL)
+        for vv in VARS:
+            print(vv)
+            for exp in experiments:
+                print(exp)
+                if "rmLC" in exp:
+                    exp_read = 'amip-lfmip-rmLC'
+                elif "pdLC" in exp:
+                    exp_read = 'amip-lfmip-pdLC'
+                else:
+                    exp_read = exp
+                path = cnst.lmcs_drive+'CMIP6/LS3MIP/'+exp_read+'/'+vv+'_*_'+MODEL+'*.nc'
+                opath = cnst.lmcs_drive+'CMIP6/LS3MIP/slices_30y/'+exp+'/'
+                outpath = opath + vv+'_30y_'+mtag+'_'+str(experiments[exp][0])+'-'+str(experiments[exp][1])+'_'+MODEL+'.nc'
 
-            if os.path.isfile(outpath):
-                print('File exists, continue')
-                continue
-            try:
-                ds = xr.open_mfdataset(path, use_cftime=True)
-            except:
-                print('FIle not found, continue', path)
-                continue
-            ds = ds.sel(time=(ds['time.year']>=experiments[exp][0]) & (ds['time.year']<=experiments[exp][1]))
-            #ipdb.set_trace()
-            if mtag == 'DJF':
-                ds = ds.sel(time=(ds['time.month']>=12) | (ds['time.month']<=2))
-            if mtag == 'JJA':
-               ds = ds.sel(time=(ds['time.month']>=6) & (ds['time.month']<=8))
-            if mtag == 'MAM':
-               ds = ds.sel(time=(ds['time.month']>=3) & (ds['time.month']<=5))
-            if mtag == 'SON':
-               ds = ds.sel(time=(ds['time.month']>=9) & (ds['time.month']<=11))
-            uni = np.unique(ds['time.year'])
-            print(exp, 'year slice', uni[0],uni[-1])
-            print('months', np.unique(ds['time.month']))
-            ds = ds.mean('time')
-            comp = dict(zlib=True, complevel=5)
-            encoding = {var: comp for var in ds.data_vars}
+                if os.path.isfile(outpath):
+                    print('File exists, continue')
+                    continue
+                try:
+                    ds = xr.open_mfdataset(path, use_cftime=True)
+                except:
+                    print('FIle not found, continue', path)
+                    continue
+                ds = ds.sel(time=(ds['time.year']>=experiments[exp][0]) & (ds['time.year']<=experiments[exp][1]))
+                #ipdb.set_trace()
+                if mtag == 'DJF':
+                    ds = ds.sel(time=(ds['time.month']>=12) | (ds['time.month']<=2))
+                if mtag == 'JJA':
+                   ds = ds.sel(time=(ds['time.month']>=6) & (ds['time.month']<=8))
+                if mtag == 'MAM':
+                   ds = ds.sel(time=(ds['time.month']>=3) & (ds['time.month']<=5))
+                if mtag == 'SON':
+                   ds = ds.sel(time=(ds['time.month']>=9) & (ds['time.month']<=11))
+                uni = np.unique(ds['time.year'])
+                print(exp, 'year slice', uni[0],uni[-1])
+                print('months', np.unique(ds['time.month']))
+                ds = ds.mean('time')
+                comp = dict(zlib=True, complevel=5)
+                encoding = {var: comp for var in ds.data_vars}
 
 
-            ds.to_netcdf(outpath, mode='w', encoding=encoding, format='NETCDF4')
-            print('Written', outpath)
-            del ds
+                ds.to_netcdf(outpath, mode='w', encoding=encoding, format='NETCDF4')
+                print('Written', outpath)
+                del ds
 
